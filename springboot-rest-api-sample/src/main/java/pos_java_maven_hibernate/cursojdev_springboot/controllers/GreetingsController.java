cf356d8382eca4ee18f2e17e0d98f010a087c9ea
TheophanesCampos <theophanes.asos@gmail.com>
2021-10-18 21:33:15 -0300

    configurar BD e criar classe usuario

--- springboot-rest-api-sample/src/main/java/pos_java_maven_hibernate/cursojdev_springboot/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/pos_java_maven_hibernate/cursojdev_springboot/controllers/GreetingsController.java
@@ -23,4 +23,10 @@ public class GreetingsController {
     public String greetingText(@PathVariable String name) {
         return "Curso Spring Boot API: " + name + "!!";
     }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method =  RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo (@PathVariable String nome) {
+    	return "Olá mundo! Bem vindo " + nome +"!";
+    }
 }
